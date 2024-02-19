cb01c0da6af647158211a1d2aea2def04351ad33
Edwin Wisze <edwin@wisze.org>
2024-02-19 22:40:22 +0100

    planeetnaam gebogen over baan

--- ptolemeus.py
+++ ptolemeus.py
@@ -8,33 +8,53 @@ from skyfield.api import load, wgs84, N, E, W, S
 from skyfield.api import position_of_radec, load_constellation_map
 from skyfield.framelib import ecliptic_frame
 
-def teken_planeet(afstand,phi,d,naam):
-   x = (afstand-d/2)*math.sin(math.radians(phi))
-   y = (afstand-d/2)*math.cos(math.radians(phi))
-   print ("   Positie",x,y)
+def teken_aarde(straal):
+   draw.ellipse([(sfeer_x-straal,sfeer_y-straal),(sfeer_x+straal,sfeer_y+straal)],(0,250,0),(0,0,0),1)
+
+def teken_planeet(straal,phi,d,naam):
+   x = (straal-d/2)*math.sin(math.radians(phi))
+   y = (straal-d/2)*math.cos(math.radians(phi))
+   # print ("   Positie",x,y)
    lcenter = 80
    sfeer = Image.new("RGBA", (lcenter*2,lcenter*2), (0, 0, 0, 0))
    label = ImageDraw.Draw(sfeer)
-   draw.arc([(sfeer_x-afstand,sfeer_y-afstand),(sfeer_x+afstand,sfeer_y+afstand)],0,360,(50,50,250),d)
+   draw.arc([(sfeer_x-straal,sfeer_y-straal),(sfeer_x+straal,sfeer_y+straal)],0,360,(0,0,250),d)
    label.ellipse([(lcenter-d/2,lcenter-d/2),(lcenter+d/2,lcenter+d/2)],(250,250,100),(0,0,0),1)
-   label.text((lcenter+d/2,lcenter), naam, anchor="lm", font=schwabacher)
+   # label.text((lcenter+d/2,lcenter), naam, anchor="lm", font=schwabacher)
    sfeer_gedraaid = sfeer.rotate((0.0-phi),resample=Image.BILINEAR,expand=False,center=(lcenter,lcenter))
-   # sfeer_gedraaid.show()
    mask = sfeer_gedraaid.split()[3]
    ikoon.paste(sfeer_gedraaid,(int(sfeer_x+x-lcenter+0.5),int(sfeer_y-y-lcenter+0.5)),mask)
+   teken_tekst(straal,phi,d,naam)
+
+def teken_tekst(straal,hoek,d,tekst):
+   lbox = 20 # grootte van plaatje van een letter
+   omtrek  = 2.0*3.14159*(straal+d)  # omtrek in pixels van het centrum van de tekst, ongeveer
+   h = hoek + 360*d/omtrek
+   for l in tekst:
+      x = (straal-d/2)*math.sin(math.radians(h))
+      y = (straal-d/2)*math.cos(math.radians(h))
+      lbeeld = Image.new("RGBA", (lbox,lbox), (0, 0, 0, 0))
+      letter = ImageDraw.Draw(lbeeld)
+      letter.text((lbox/2,lbox/2), l, anchor="mm", font=schwabacher)
+      lt = letter.textlength(l,schwabacher)
+      lbeeld_gedraaid = lbeeld.rotate(-h,resample=Image.BILINEAR,expand=False,center=(lbox/2,lbox/2))
+      # beeld_gedraaid.show()
+      mask = lbeeld_gedraaid.split()[3]
+      ikoon.paste(lbeeld_gedraaid,(int(sfeer_x+x-lbox/2),int(sfeer_y-y-lbox/2)),mask)
+      h += 360.0*lt/omtrek
 
 # Sterrenbeelden vanaf lentepunt met icoon uit Astrologicus font
 sterrenbeelden = {'Ram':'A','Stier':'B','Tweelingen':'C','Krab':'D',
                   'Leeuw':'E','Maagd':'F','Weegschaal':'G','Kreeft':'H',
                   'Boogschutter':'I','Steenbok':'J','Waterman':'K','Vissen':'L'}   
-def teken_zodiac(afstand,phi,d):
+def teken_dierenriem(straal,phi,d):
    sb = 100 # 100 pixels voor sterrenbeeld
    isterrenbeeld = 1
-   draw.arc([(sfeer_x-afstand,sfeer_y-afstand),(sfeer_x+afstand,sfeer_y+afstand)],0,360,(100,100,250),d)
+   draw.arc([(sfeer_x-straal,sfeer_y-straal),(sfeer_x+straal,sfeer_y+straal)],0,360,(100,100,250),d)
    for s in sterrenbeelden:
       hoeks = phi - isterrenbeeld*30.0
-      x = (afstand-d/2)*math.sin(math.radians(hoeks))
-      y = (afstand-d/2)*math.cos(math.radians(hoeks))
+      x = (straal-d/2-4)*math.sin(math.radians(hoeks))
+      y = (straal-d/2-4)*math.cos(math.radians(hoeks))
       print (s,hoeks,x,y)
       sterrenbeeld = Image.new("RGBA", (sb,sb), (0, 0, 0, 0))
       teken = ImageDraw.Draw(sterrenbeeld)
@@ -42,11 +62,11 @@ def teken_zodiac(afstand,phi,d):
       # sterrenbeeld.show()
       sterrenbeeld_gedraaid = sterrenbeeld.rotate((0.0-hoeks),resample=Image.BILINEAR,expand=False,center=(sb/2,sb/2))
       mask = sterrenbeeld_gedraaid.split()[3]
-      ikoon.paste(sterrenbeeld_gedraaid,(int(sfeer_x+x-sb/2+0.5),int(sfeer_y-y-sb/2+0.5)),mask)
+      ikoon.paste(sterrenbeeld_gedraaid,(int(sfeer_x+x-sb/2),int(sfeer_y-y-sb/2)),mask)
       isterrenbeeld += 1
 
-schwabacher = ImageFont.truetype("lib/OfenbacherSchwabCAT.ttf",size=14)
-astrologicus = ImageFont.truetype("lib/Astrologicus.ttf",size=36)
+schwabacher = ImageFont.truetype("lib/OfenbacherSchwabCAT.ttf",size=20)
+astrologicus = ImageFont.truetype("lib/Astrologicus.ttf",size=40)
 ikoon = Image.new("RGB",(480,800), (250,250,150))
 sfeer_x = ikoon.width/2
 sfeer_y = ikoon.height - ikoon.width/2
@@ -70,15 +90,17 @@ print ('lentepunt staat in', constellation_at(lentepunt))
 # Laad de baangegevens van de planeten, JPL ephemeris DE421
 eph = load('de421.bsp')
 aarde = eph['earth']
-planets = {'Maan':      'Moon',
-           'Mercurius': 'Mercury',
-           'Venus':     'Venus',
-           'Zon':       'Sun',
-           'Mars':      'Mars',
-           'Jupiter':   'Jupiter barycenter',
-           'Saturnus':  'Saturn barycenter'}
+planets = {'maan':      'Moon',
+           'mercurius': 'Mercury',
+           'venus':     'Venus',
+           'zon':       'Sun',
+           'mars':      'Mars',
+           'jupiter':   'Jupiter barycenter',
+           'saturnus':  'Saturn barycenter'}
 
-r = 50
+r = 38
+teken_aarde(r)
+r += 1
 # Bereken posities van de planeten in het ecliptisch assenstelsel
 for p in planets:
    planeetpositie = aarde.at(t).observe(eph[planets[p]])
@@ -86,15 +108,15 @@ for p in planets:
    azimut_equatoriaal = (hoek_lentepunt-lon.degrees)%360
    print (p, constellation_at(planeetpositie), lon.degrees, azimut_equatoriaal)
    # Teken de cirkel met planeet
-   if (p == 'Zon' or p == 'Maan'):
+   if (p == 'zon' or p == 'maan'):
       w = 35
    else:
       w = 15
-   r += w+2;
+   r += w+1;
    teken_planeet(r,azimut_equatoriaal,w,p)
-w = 30
-r += w+2
-teken_zodiac(r,hoek_lentepunt,w)
+w = 35
+r += w+1
+teken_dierenriem(r,hoek_lentepunt,w)
    
 ikoon.save('ptolemeus.png')
 ikoon.show()
